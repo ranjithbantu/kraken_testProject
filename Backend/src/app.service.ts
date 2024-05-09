e3cc570e1152060ec6d412f9e584704a249e3d24
Ranjith <31557329+ranjithbantu@users.noreply.github.com>
2024-05-09 06:49:42 -0700

    Fix pair problem
    It is addressed the issue where the ‘pair’ parameter was sometimes undefined in the makeResponseData function, which led to the TypeError during the split() operation

--- Backend/src/app.service.ts
+++ Backend/src/app.service.ts
@@ -22,30 +22,36 @@ export class AppService {
   }
 
   async makeResponseData(pair: string, name: string): Promise<string> {
+    if (!pair || typeof pair !== 'string') {
+      throw new Error('Invalid or missing pair parameter');
+    }
     const typeArray = pair.split(',');
     const nameArray = name.split(',');
     const resultData: any[] = [];
     for (let i = 0; i < typeArray.length; i++) {
-      const orderBook = await this.getOrderBook(typeArray[i]);
-      const { bids, asks } = orderBook.result[nameArray[i]];
-      const bestBid = parseFloat(bids[0][0]);
-      const bestAsk = parseFloat(asks[0][0]);
-      const spread = bestAsk - bestBid;
-      const midPrice = (bestBid + bestAsk) / 2;
-      const spredPercentage = (spread / midPrice) * 100;
-      const tempData = {
-        name: typeArray[i],
-        bids: bids,
-        asks: asks.reverse(),
-        spread: spread,
-        midPrice: midPrice,
-        spredPercentage: spredPercentage
+      try {
+        const orderBook = await this.getOrderBook(typeArray[i]);
+        const { bids, asks } = orderBook.result[nameArray[i]];
+        const bestBid = parseFloat(bids[0][0]);
+        const bestAsk = parseFloat(asks[0][0]);
+        const spread = bestAsk - bestBid;
+        const midPrice = (bestBid + bestAsk) / 2;
+        const spredPercentage = (spread / midPrice) * 100;
+        const tempData = {
+          name: typeArray[i],
+          bids: bids,
+          asks: asks.reverse(),
+          spread: spread,
+          midPrice: midPrice,
+          spredPercentage: spredPercentage
+        }
+        resultData.push(tempData);
+      } catch (error) {
+        console.error(`Error fetching data for ${typeArray[i]}: ${error.message}`);
       }
-      resultData.push(tempData);
     }
     return JSON.stringify(resultData);
   }
-
   async emitKrakenData(data: string, name: string) {
     this.webSocketService.broadcastMessage(await this.makeResponseData(data, name));
   }
